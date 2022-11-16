2d01c29ce7053e4bf62e05dc6849ef44f584e739
Ethan <ethanwk@ad.unc.edu>
2022-11-16 14:40:12 -0500

    no endpoint added

--- server.js
+++ server.js
@@ -7,14 +7,16 @@ import express from "express"
 const args = minimist(process.argv.slice(2))
 
 let port = args.port? args.port:5000
-const app = express()       //create express variable for calling reqs/post
+const app = express() //create express variable for calling reqs/post
 
 //Check endpoint
 app.get('/app', function (req, res) {
-    //console.log("Hello Console")
     res.status(200).send("200 OK")
 })
 
+//Non-endpoint
+app.get('*', function (req, res) {
+    res.status(404).send("404 NOT FOUND")
+})
 
 app.listen(port)
-//console.log(port)
