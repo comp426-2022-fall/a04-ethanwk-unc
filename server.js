2e51ec01a7f41475a7bfd1b29f494470ef0a346f
Ethan <77816049+ethanwk-unc@users.noreply.github.com>
2022-11-16 11:47:25 -0500

    tying out port default

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+import { roll } from "./lib/roll.js"
+import minimist from "minimist"
+import express from "express"
+
+const args = minimist(process.argv.slice(2));
+
+let port = args.port? args.port:5000;
+console.log(port)
+
+/*
+let sides = args.sides? args.sides:6; //mark optional type
+let dice = args.dice? args.dice:2;
+let rolls = args.rolls? args.rolls:1;
+
+var results = roll(sides, dice, rolls);
+console.log(JSON.stringify(results))  
+*/
