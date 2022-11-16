c6cc90c8898370b06490fc9a276ade91417d2145
Ethan <ethanwk@ad.unc.edu>
2022-11-16 14:36:28 -0500

    Testing check endpoint

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ import { roll } from "./lib/roll.js"
 import minimist from "minimist"
 import express from "express"
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2))
 
-let port = args.port? args.port:5000;
+let port = args.port? args.port:5000
 const app = express()       //create express variable for calling reqs/post
 
-app.get('/', function (req, res) {
-    console.log("Hello Console")
-    res.send('Hello User');
+//Check endpoint
+app.get('/app', function (req, res) {
+    //console.log("Hello Console")
+    res.status(200).send("200 OK")
 })
+
 
 app.listen(port)
 //console.log(port)
-
-//create app var for express function
-
